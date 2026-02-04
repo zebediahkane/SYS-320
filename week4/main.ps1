23ea60313c815180a18b596dbb2177f3f4610033
zebediahkane <zebediah.kane@mymail.champlain.edu>
2026-02-04 14:57:06 -0500

    Small change

--- week4/main.ps1
+++ week4/main.ps1
@@ -1,4 +1,10 @@
 ﻿. .\ApacheLogs2.ps1
 
+$logData = parseLog("C:\xampp\apache\logs\access.log")
+
+# Prints every log
+Write-Host ($logData | Format-Table -AutoSize | Out-String)
+
+# Filtered
 parseLog("C:\xampp\apache\logs\access.log") | Where-Object `
 { ($_.Page -like '*page2.html') -and ($_.Referrer -like "*index.html") } | Format-Table -AutoSize | Out-String
